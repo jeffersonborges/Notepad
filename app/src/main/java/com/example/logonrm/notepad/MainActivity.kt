4a345026ecd26af552b510ce7fa452364cf87dd8
jeffersonborges <jefferson.gmb@gmail.com>
2018-03-03 10:25:42 -0300

    Criação do menu

--- app/src/main/java/com/example/logonrm/notepad/MainActivity.kt
+++ app/src/main/java/com/example/logonrm/notepad/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.logonrm.notepad
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_principal, menu)
+        return true
     }
 }
